50473b0c82107e940adf9ac2402e058f9068192e
tranthao <tran.thao.primelabo@gmail.com>
2022-03-25 21:17:09 +0700

    pokemon memorise

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,83 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main-screen
+    v-if="statusMatch === 'default'"
+    @onStart="onHandleBeforeStart"
+  ></main-screen>
+  <interact-screen
+    v-if="statusMatch === 'match'"
+    :cardsContext="settings.cardsContext"
+    @onFinish="onGetResult"
+  ></interact-screen>
+  <result-screen
+    v-if="statusMatch === 'result'"
+    :timer="timer"
+    @onStartAgain="statusMatch = 'default'"
+  ></result-screen>
+  <p class="copyright">
+    Chơi game zui zẻ hông quạo 'QUỶ ĐÁ KHÔI - 99' -
+    <a href="https://www.facebook.com/petho1907">view here</a>
+  </p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MainScreen from "./components/MainScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import ResultScreen from "./components/ResultScreen.vue";
+import { shuffled } from "./util/array";
 export default {
-  name: 'App',
+  name: "PokemonMemorise",
   components: {
-    HelloWorld
-  }
-}
+    MainScreen,
+    InteractScreen,
+    ResultScreen,
+  },
+  data() {
+    return {
+      settings: {
+        totalOfBlocks: 0,
+        cardsContext: [],
+        startedAt: 0,
+      },
+      statusMatch: "default",
+      timer: 0,
+    };
+  },
+  methods: {
+    onHandleBeforeStart(config) {
+      this.settings.totalOfBlocks = config.totalOfBlocks;
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlocks / 2 },
+        (_, i) => i + 1
+      );
+      const secondCards = [...firstCards]; // clone array firstCards
+      const cards = [...firstCards, ...secondCards];
+      this.settings.cardsContext = shuffled(
+        // mix and shuffled array
+        shuffled(shuffled(shuffled(cards)))
+      );
+      this.settings.startedAt = new Date().getTime();
+      this.statusMatch = "match";
+    },
+    onGetResult() {
+      // get timer
+      this.timer = new Date().getTime() - this.settings.startedAt;
+      // switch result component
+      this.statusMatch = "result";
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="css" scoped>
+.copyright {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 1.5rem;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1.5rem;
+}
+.copyright a {
+  color: #f4dc26;
 }
 </style>
---
+++ src/components/CoppyRightScreen.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>
+    This game owned by Khôi Đá in Vue 3 course for begginer -
+    <a href="https://www.facebook.com/petho1907">view here</a>
+  </p>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
